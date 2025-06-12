b30d6b06ec96271cf294ff380a26c95a596a27e3
Stuart Maxwell <stuart@amanzi.nz>
2025-06-12 22:02:31 +1200

    Update for DJ Press 0.20

--- src/djpress_publish_mastodon/plugin.py
+++ src/djpress_publish_mastodon/plugin.py
@@ -1,32 +1,35 @@
+from typing import TYPE_CHECKING
 from urllib.parse import urljoin
 
 from djpress.plugins import DJPressPlugin
+from djpress.plugins.hook_registry import POST_SAVE_POST
 from mastodon import Mastodon
+
+if TYPE_CHECKING:
+    from djpress.models import Post
 
 
 class Plugin(DJPressPlugin):
     name = "djpress_publish_mastodon"
+    hooks = [(POST_SAVE_POST, "publish_post")]
 
-    def setup(self, registry):
-        registry.register_hook("post_save_post", self.publish_post)
-
-    def publish_post(self, post):
-        # Get the settings from the config dictionary
-        access_token = self.config.get("access_token")
-        instance_url = self.config.get("instance_url")
-        status_message = self.config.get("status_message")
-        base_url = self.config.get("base_url")
+    def publish_post(self, post: "Post") -> "Post":
+        # Get the settings from the settings dictionary
+        access_token = self.settings.get("access_token")
+        instance_url = self.settings.get("instance_url")
+        status_message = self.settings.get("status_message")
+        base_url = self.settings.get("base_url")
 
         # Silently fail if any of the required config is missing
         if not access_token or not instance_url or not status_message or not base_url:
-            return
+            return post
 
         # We keep track of which posts have been published to Mastodon already by adding the post id to the data with a
         # key called "published_posts". This way we can check if the post has already been published to Mastodon.
         data = self.get_data()
         published_posts = data.get("published_posts", [])
-        if post.id in published_posts:
-            return
+        if post.pk in published_posts:
+            return post
 
         try:
             mastodon = Mastodon(access_token=access_token, api_base_url=instance_url)
@@ -35,7 +38,7 @@ class Plugin(DJPressPlugin):
 
             mastodon.toot(post_content)
 
-            published_posts.append(post.id)
+            published_posts.append(post.pk)
             data["published_posts"] = published_posts
             self.save_data(data)
         except Exception:
